71ffe11959bcb4f5d1be7a064a8eb6f6faab7455
sk <sk@naver.com>
2016-09-02 13:17:13 +0900

    파일 수정

--- Git_sk_1013/src/com/bitacademy/git/HelloGit.java
+++ Git_sk_1013/src/com/bitacademy/git/HelloGit.java
@@ -5,7 +5,9 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hi git");
 		System.out.println("nice to meet youㅎㅎ");
-
+		//주석1 걍 저장만
+		//주석2 "
+		//주석 추가  ->저장 ->commit
 	}
 
 }
