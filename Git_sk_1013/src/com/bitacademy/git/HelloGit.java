67df7d1d817fb78d3b48bc3f26d0e6119e13410d
zeus <karl0@naver.com>
2016-09-02 14:27:24 +0900

    응답

--- Git_sk_1013/src/com/bitacademy/git/HelloGit.java
+++ Git_sk_1013/src/com/bitacademy/git/HelloGit.java
@@ -4,10 +4,11 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hi git");
-		System.out.println("nice to meet youㅎㅎ");
-		//주석1 걍 저장만
-		//주석2 "
-		//주석 추가  ->저장 ->commit
+		System.out.println("nice to meet you�뀕�뀕");
+		//二쇱꽍1 嫄� ���옣留�
+		//二쇱꽍2 "
+		//二쇱꽍 異붽�  ->���옣 ->commit
+		System.out.println("good to see you");
 	}
 
 }
