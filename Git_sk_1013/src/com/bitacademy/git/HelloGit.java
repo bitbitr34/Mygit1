eab7d172152ecb830b85f97a26446b12d3f7a297
sk <sk@naver.com>
2016-09-02 11:46:43 +0900

    프로젝트 공유

---
+++ Git_sk_1013/src/com/bitacademy/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.bitacademy.git;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hi git");
+		System.out.println("nice to meet youㅎㅎ");
+
+	}
+
+}
